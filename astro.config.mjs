9911160708df6fa189932054a7bb3d1f52d40cae
Milan Jaritz <milan.jaritz@gmail.com>
2023-03-21 20:22:28 +0100

    add image preprocessor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
+import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [
+    tailwind(),
+    svelte(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
