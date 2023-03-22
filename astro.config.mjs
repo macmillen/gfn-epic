18ab02a5411ab6231f82b3bf25d80cf35d8be963
Milan Jaritz <milan.jaritz@gmail.com>
2023-03-22 21:50:31 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -9,5 +10,7 @@ export default defineConfig({
     tailwind(),
     svelte(),
     image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    sitemap(),
   ],
+  site: "https://gfn-epic.com",
 });
--- src/components/icons/icon-epic-games.svelte
+++ src/components/icons/icon-epic-games.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svg
-  class={classes}
+  {...classes ? { class: classes } : {}}
   xmlns="http://www.w3.org/2000/svg"
   width="32"
   height="32"
