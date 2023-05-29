d0afa05a58200f938cc2358f7163489592d8c4a1
Milan Jaritz <milan.jaritz@gmail.com>
2023-05-29 22:59:00 +0200

    set output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  experimental: { hybridOutput: true },
   integrations: [
     tailwind(),
     svelte(),
