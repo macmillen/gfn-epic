8adb68b6ee5f9098d28f4faf19a7967121042d73
Milan Jaritz <milan.jaritz@gmail.com>
2023-11-16 21:34:55 +0100

    add buy me a coffee link

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,4 +1,7 @@
-<a href="/" class="flex font-black text-green-500 text-3xl select-none">
+<a
+  href="/"
+  class="flex font-black text-green-500 sm:text-3xl text-xl select-none"
+>
   <span class="-rotate-6">GFN</span>
   <span class="text-zinc-100 mt-2 ml-1 rotate-3">EPIC</span>
 </a>
