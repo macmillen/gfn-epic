fecf69966829075f249114ce4ffb1f072fae3e6f
Milan Jaritz <milan.jaritz@gmail.com>
2024-01-12 11:45:19 +0100

    add fetch new epic games script to generate all

--- scripts/fetch-new-epic-games.js
+++ scripts/fetch-new-epic-games.js
@@ -1,51 +1,53 @@
 import { readJson, writeJson } from "./utils.js";
 
-const response = await fetch(
-  encodeURI(
-    "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US"
-  ),
-  {
-    method: "GET",
-  }
-);
-
-const data = await response.json();
-
-const elements = data.data.Catalog.searchStore.elements;
-
-const freeGames = elements
-  .map(({ title, promotions }) => {
-    const currentOffer = promotions.promotionalOffers[0];
-    const upcomingOffer = promotions.upcomingPromotionalOffers[0];
-
-    if (currentOffer) {
-      const offer = currentOffer.promotionalOffers[0];
-      return { title, fromDate: offer.startDate, untilDate: offer.endDate };
+export const fetchNewEpicGames = async () => {
+  const response = await fetch(
+    encodeURI(
+      "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US"
+    ),
+    {
+      method: "GET",
     }
-    if (upcomingOffer) {
-      const offer = upcomingOffer.promotionalOffers[0];
-      return { title, fromDate: offer.startDate, untilDate: offer.endDate };
+  );
+
+  const data = await response.json();
+
+  const elements = data.data.Catalog.searchStore.elements;
+
+  const freeGames = elements
+    .map(({ title, promotions }) => {
+      const currentOffer = promotions.promotionalOffers[0];
+      const upcomingOffer = promotions.upcomingPromotionalOffers[0];
+
+      if (currentOffer) {
+        const offer = currentOffer.promotionalOffers[0];
+        return { title, fromDate: offer.startDate, untilDate: offer.endDate };
+      }
+      if (upcomingOffer) {
+        const offer = upcomingOffer.promotionalOffers[0];
+        return { title, fromDate: offer.startDate, untilDate: offer.endDate };
+      }
+    })
+    .reverse();
+
+  const dataObjects = readJson("../src/data/generated/data-objects.json");
+
+  const lastDataObjects = dataObjects.slice(0, freeGames.length);
+
+  let newFreeGamesToBeAdded = [];
+
+  for (const freeGame of freeGames) {
+    const dataObject = lastDataObjects.find((v) => v.title === freeGame.title);
+    if (dataObject) {
+      continue;
+    } else {
+      console.log(freeGame);
+      newFreeGamesToBeAdded.push(freeGame);
     }
-  })
-  .reverse();
-
-const dataObjects = readJson("../src/data/generated/data-objects.json");
-
-const lastDataObjects = dataObjects.slice(0, freeGames.length);
-
-let newFreeGamesToBeAdded = [];
-
-for (const freeGame of freeGames) {
-  const dataObject = lastDataObjects.find((v) => v.title === freeGame.title);
-  if (dataObject) {
-    continue;
-  } else {
-    console.log(freeGame);
-    newFreeGamesToBeAdded.push(freeGame);
   }
-}
 
-writeJson("../src/data/generated/data-objects.json", [
-  ...newFreeGamesToBeAdded,
-  ...dataObjects,
-]);
+  writeJson("../src/data/generated/data-objects.json", [
+    ...newFreeGamesToBeAdded,
+    ...dataObjects,
+  ]);
+};
--- scripts/generate-all.js
+++ scripts/generate-all.js
@@ -1,8 +1,10 @@
 import { fetchGameData } from "./fetch-game-data.js";
 import { fetchGameIds } from "./fetch-game-ids.js";
 import { fetchGfnData } from "./fetch-gfn-data.js";
+import { fetchNewEpicGames } from "./fetch-new-epic-games.js";
 import { generateFinalData } from "./generate-final-data.js";
 
+await fetchNewEpicGames();
 await fetchGfnData();
 await fetchGameIds();
 await fetchGameData();
