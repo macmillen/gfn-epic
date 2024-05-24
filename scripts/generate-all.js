4cd5eaa4750c7d2735587edfbb9952b8d62c8f0f
Milan Jaritz <milan.jaritz@gmail.com>
2024-05-24 13:13:25 +0200

    remove fetch new epic games for generate all script

--- scripts/generate-all.js
+++ scripts/generate-all.js
@@ -1,10 +1,8 @@
 import { fetchGameData } from "./fetch-game-data.js";
 import { fetchGameIds } from "./fetch-game-ids.js";
 import { fetchGfnData } from "./fetch-gfn-data.js";
-import { fetchNewEpicGames } from "./fetch-new-epic-games.js";
 import { generateFinalData } from "./generate-final-data.js";
 
-await fetchNewEpicGames();
 await fetchGfnData();
 await fetchGameIds();
 await fetchGameData();
