56ce28a3d5abdf4cbbc1d002d230b680c12b6511
Milan Jaritz <milan.jaritz@gmail.com>
2024-12-06 14:19:10 +0100

    use fontsource

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: "Inter",
+      sans: "Inter Variable",
     },
     extend: {
       colors: {
