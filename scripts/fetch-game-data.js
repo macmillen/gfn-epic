0aa6eba627d5c77840ca1628a99e9bcb3742cdc9
Milan Jaritz <milan.jaritz@gmail.com>
2025-09-04 13:13:28 +0200

    chunk game list for more than 500 entries

--- scripts/fetch-game-data.js
+++ scripts/fetch-game-data.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { chunk } from "es-toolkit";
 import _igdb from "igdb-api-node";
 import { downloadFile, printSuccess, readJson, writeJson } from "./utils.js";
 
@@ -15,69 +16,77 @@ const client = igdb(
 export const fetchGameData = async () => {
   const gameIdMap = readJson("../src/data/generated/game-id-map.json");
 
-  const ids = Object.values(gameIdMap)
-    .filter((v) => v !== null)
-    .join(",");
+  const id500Chunks = chunk(
+    Object.values(gameIdMap).filter((v) => v !== null),
+    500
+  ).map((c) => c.join(","));
 
-  const response = await client
-    .fields([
-      "name",
-      "cover.*",
-      "first_release_date",
-      "genres.*",
-      "total_rating",
-      "alternative_names.name",
-      "summary",
-      "websites.*",
-      "age_ratings.*",
-    ])
-    .where(`id = (${ids})`)
-    .limit(500)
-    .request("https://api.igdb.com/v4/games");
+  let result = {};
 
-  const getCover = (responseData, replaceWith) => {
-    return responseData.cover
-      ? "https:" + responseData.cover.url.replace("t_thumb", replaceWith)
-      : undefined;
-  };
+  for await (const id500Chunk of id500Chunks) {
+    const response = await client
+      .fields([
+        "name",
+        "cover.*",
+        "first_release_date",
+        "genres.*",
+        "total_rating",
+        "alternative_names.name",
+        "summary",
+        "websites.*",
+        "age_ratings.*",
+      ])
+      .where(`id = (${id500Chunk})`)
+      .limit(500)
+      .request("https://api.igdb.com/v4/games");
 
-  const result = response.data.reduce((acc, responseData) => {
-    const coverSmallLink = getCover(responseData, "t_cover_small");
-    const coverBigLink = getCover(responseData, "t_cover_big");
-
-    downloadFile(
-      coverSmallLink,
-      `${responseData.id}_small.jpg`,
-      "public/game-images"
-    );
-    downloadFile(
-      coverBigLink,
-      `${responseData.id}_big.jpg`,
-      "public/game-images"
-    );
-
-    return {
-      ...acc,
-      [responseData.id]: {
-        id: responseData.id,
-        cover: `/game-images/${responseData.id}_small.jpg`,
-        coverBig: getCover(responseData, "t_cover_big"),
-        alternative_names: responseData.alternative_names,
-        first_release_date: responseData.first_release_date
-          ? new Date(responseData.first_release_date * 1000).toISOString()
-          : undefined,
-        genres: responseData.genres?.map(({ name }) => name),
-        name: responseData.name,
-        rating: responseData.total_rating,
-        summary: responseData.summary,
-        age_ratings: responseData.age_ratings,
-        websites: responseData.websites?.map((d) => ({
-          url: d.url,
-          category: d.category,
-        })),
-      },
+    const getCover = (responseData, replaceWith) => {
+      return responseData.cover
+        ? "https:" + responseData.cover.url.replace("t_thumb", replaceWith)
+        : undefined;
     };
-  }, {});
+
+    result = {
+      ...result,
+      ...response.data.reduce((acc, responseData) => {
+        const coverSmallLink = getCover(responseData, "t_cover_small");
+        const coverBigLink = getCover(responseData, "t_cover_big");
+
+        downloadFile(
+          coverSmallLink,
+          `${responseData.id}_small.jpg`,
+          "public/game-images"
+        );
+        downloadFile(
+          coverBigLink,
+          `${responseData.id}_big.jpg`,
+          "public/game-images"
+        );
+
+        return {
+          ...acc,
+          [responseData.id]: {
+            id: responseData.id,
+            cover: `/game-images/${responseData.id}_small.jpg`,
+            coverBig: getCover(responseData, "t_cover_big"),
+            alternative_names: responseData.alternative_names,
+            first_release_date: responseData.first_release_date
+              ? new Date(responseData.first_release_date * 1000).toISOString()
+              : undefined,
+            genres: responseData.genres?.map(({ name }) => name),
+            name: responseData.name,
+            rating: responseData.total_rating,
+            summary: responseData.summary,
+            age_ratings: responseData.age_ratings,
+            websites: responseData.websites?.map((d) => ({
+              url: d.url,
+              category: d.category,
+            })),
+          },
+        };
+      }, {}),
+    };
+  }
 
   writeJson("../src/data/generated/igdb-game-data-map.json", result);
 
